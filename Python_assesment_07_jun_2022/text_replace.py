e955d9f25d679bff50bd2a8012d70049d3bcafa5
Supreeth Shetty <shetty.supreeth95@gmail.com>
2022-06-07 16:14:15 +0530

    added

--- Python_assesment_07_jun_2022/text_replace.py
+++ Python_assesment_07_jun_2022/text_replace.py
@@ -20,13 +20,15 @@ class ReplaceContent():
                 file_data = file_data.replace(old_string, new_string) # Replace old_string with new_string in file_data
                 with open(self.text_file, 'w') as f: # Open file for writing
                     f.write(file_data)
-                logging.info(f"Replaced all occurrences of '{old_string}' with '{new_string}' in file '{self.text_file}'")
-                logging.info(f"The file content after replacement:{file_data}")
+                    logging.info(f"Replaced all occurrences of '{old_string}' with '{new_string}' in file '{self.text_file}'")
+                    logging.info(f"The file content after replacement:{file_data}")
             else:
                 logging.info(f"No occurrences of '{old_string}' found in file '{self.text_file}'")
         
         except Exception as e:
             logging.error(e)
 
-Content = ReplaceContent("example1.txt")
+Content = ReplaceContent("example.txt")
 Content.replace_string_in_file("placement", "screening")
+
+print("Please check the logs.log file for more details")
